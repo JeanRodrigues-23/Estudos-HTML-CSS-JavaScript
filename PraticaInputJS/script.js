d777d3a4547175f48c495dcd813998c2728a7ab1
JeanRodrigues-23 <jeantrabalhodf@gmail.com>
2025-05-29 02:06:53 -0300

    Atualizando a pasta PraticaInputJS

--- PraticaInputJS/script.js
+++ PraticaInputJS/script.js
@@ -1,16 +1,17 @@
 function adicionarTarefa() {
 
     let inputTarefa = document.getElementById("inputTarefa"); // Cria a variável inputTarefa e guarda nela o input com id="inputTarefa"
-    let tarefa = inputTarefa.value; // Cria a variável "tarefa" e insere o valor guardado no input de id="inputTarefa"
+    let tarefa = inputTarefa.value.trim(); // Cria a variável "tarefa" e insere o valor guardado no input de id="inputTarefa". Obs: o "trim()" retira os espaços em branco do começo e fim do código
+    let mensagem = document.getElementById("mensagem"); //Salva o elemento com id="mensagem" dentro da variável "mensagem"
 
     if (tarefa === "") {
-        document.getElementById("mensagem").textContent = "Por favor, digite uma tarefa antes de adicionar."
-        document.getElementById("mensagem").style.color = "red";
+        mensagem.textContent = "Por favor, digite uma tarefa antes de adicionar."
+        mensagem.style.color = "#A34743";
         return;
     }; // Confere se input está vazio e não adiciona, caso esteja
 
-    let mensagem = "Tarefa adicionada com sucesso!"; // Cria a variável "mensagem" e insere conteúdo nela
-    document.getElementById("mensagem").textContent = mensagem; // Acrescenta o contúdo da variável "mensagem" ao parágrafo com id="mensagem"
+    mensagem.textContent = "Tarefa adicionada com sucesso!"; // Adiciona conteúdo na variável "mensagem"
+    mensagem.style.color = "#28A745";
 
     let listaTarefas = document.getElementById("listaTarefas"); // Cria a variável "listaTarefas" e atribui a ela o ul com id="listaTarefas"
     let novaTarefa = document.createElement("li"); // Criar a variável "novaTarefa" e atribui a ela a criação de um li (list item)
